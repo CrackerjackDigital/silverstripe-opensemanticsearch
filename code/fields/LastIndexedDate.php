34fdc8892f16d94d5b4ce3ee886eb5b88281ecb4
Steve Wake <steve@moveforward.co.nz>
2017-09-11 12:23:21 +1200

    only add LastIndexedDate it fields has a tab

--- code/fields/LastIndexedDate.php
+++ code/fields/LastIndexedDate.php
@@ -1,10 +1,25 @@
 <?php
 namespace OpenSemanticSearch\Fields;
 
+use FieldList;
 use Modular\Field;
 use Modular\Types\DateTimeType;
 
 class LastIndexedDate extends Field implements DateTimeType {
 	const Name = 'OSSLastIndexedDate';
 
+	public function updateCMSFields( FieldList $fields ) {
+		parent::updateCMSFields( $fields );
+		if ( $this->owner->ParentID && \Permission::check( 'ADMIN' ) ) {
+			if ($fields->hasTabSet()) {
+				$fields->addFieldToTab(
+					'Root.Admin',
+					new \TextField( self::Name )
+				);
+
+			}
+		} else {
+			$fields->removeByName( self::Name );
+		}
+	}
 }
